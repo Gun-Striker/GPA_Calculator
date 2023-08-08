5fa18d52f3b892eee7105cfc68e5c38c6ede3e70
Gun Striker <81180347+Gun-Striker@users.noreply.github.com>
2023-08-09 02:59:08 +0600

    Simplified the code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,33 +2,17 @@ fun main() {
     println("Enter your number:")
     val num:Float = readln().toFloat()
 
-    if (num > 100) {
-        println("Invalid Number! Enter a number between 0-100.")
-    }
-    else if(num>=80) {
-        println("Grade: A+ \nGPA: 5.00")
-    }
-    else if (num>=70){
-        println("Grade: A \nGPA: 4.00")
-    }
+    if (num > 100) println("Invalid Number! Enter a number between 0-100.")
+    else if(num>=80) println("Grade: A+ \nGPA: 5.00")
+    else if (num>=70) println("Grade: A \nGPA: 4.00")
 
-    else if (num>= 60) {
-        println("Grade: A- \nGPA: 3.5")
-    }
+    else if (num>= 60) println("Grade: A- \nGPA: 3.5")
 
-    else if (num>=50) {
-        println("Grade: B \nGPA: 3.00")
-    }
+    else if (num>=50) println("Grade: B \nGPA: 3.00")
 
-    else if (num>=40){
-        println("Grade: C \nGPA: 2.00")
-    }
+    else if (num>=40) println("Grade: C \nGPA: 2.00")
 
-    else if (num>=33) {
-        println("Grade: D \nGPA: 1.00")
-    }
+    else if (num>=33) println("Grade: D \nGPA: 1.00")
 
-    else {
-        println("Grade: F \nGPA: 0.00 \n \nYOU FAILED!")
-    }
+    else println("Grade: F \nGPA: 0.00 \n \nYOU FAILED!")
 }
